1dcac168096e7606c546106b4d96f8a1f2150c9b
Shivam Mishra <smishra06542@gmail.com>
2022-11-08 13:41:20 +0530

    code_fixed_shivam

--- login.js
+++ login.js
@@ -2,10 +2,13 @@
 let num=23;
 let factor=0;
 for(i=0;i<=num;i++){
-    factor++;
+   if(num%i==0){
+    factor++
+   }
 }if(factor==2){
     console.log("it's prime");
 }else 
 {
     console.log("it's not prime");
 }
+
